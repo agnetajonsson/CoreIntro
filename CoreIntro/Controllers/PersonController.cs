5b6b42fde216c225cebb19ebf828518f84a3aab1
DZM <d.bjorlingh@gmail.com>
2019-05-10 16:04:39 +0200

    Add project files.

---
+++ CoreIntro/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreIntro.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        public IActionResult Index(string Name, int Salary)
+        {
+            var model = new Person(Name, Salary);
+
+            return View(model);
+        }
+
+
+        public IActionResult AllPersons()
+        {
+
+            var model = new List<Person>
+            {
+                new Person("Kalle", 10000),
+                new Person("Lisa", 15000)
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ CoreIntro/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreIntro.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+       // [Range(0, int.MaxValue)]
+        public int Salary { get; set; }
+
+        public Person(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public Person()
+        {
+
+        }
+    }
+}
